d3cc224bf6602d92f020e11f82a01e97f0f8194f
quimgc <quimgonzalez@iesebre.com>
2018-05-07 20:24:32 +0200

    Sync

--- resources/views/pdf/chat.blade.php
+++ resources/views/pdf/chat.blade.php
@@ -5,14 +5,23 @@
 <ul>
     <?php foreach($chat->messages as $message): ?>
         <div>
-            <b>{{$message->user->name}}</b>: {{$message->body}} <br>
+            <b>{{$message->user->name}}</b>: {{$message->body}} <div id="date">{{$message->formatted_created_at_date}}</div><br>
         </div>
     <?php endforeach; ?>
 </ul>
 
 <style>
     div{
+        display: flex;
         padding: 10px;
     }
+
     div:nth-child(odd) { background: #ddd }
+
+    #date{
+        padding: 0px;
+        margin-left: auto;
+        color: grey;
+        font-size: 12px;
+    }
 </style>
